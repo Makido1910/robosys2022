bcce50988d8a75e0be8b52a696d9ef5473e46103
Yusuke Makido <s21c1107za@s.chibakoudai.jp>
2022-11-27 18:34:43 +0900

    fix test

--- test.bash
+++ test.bash
@@ -36,6 +36,10 @@ out=$(echo | ./mul)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo あ い う > nums | cat nums | tr ' ' '\n' | ./loc)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo ok
 exit $res
 
